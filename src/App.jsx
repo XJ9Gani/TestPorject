c171177e0c4eb6555e98c93bb70f2cfd689f59a8
Ваше_имя <_email>
2024-12-10 16:17:34 +0500

    final v

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import AppRouter from "./components/AppRouter";
 import Footer from "./components/Footer";
+import { useState, useEffect } from "react";
+import "./style/Global.scss";
 
 export default function App() {
+  const [user, setUser] = useState();
+  const location = useLocation();
+
+  useEffect(() => {
+    const userData = localStorage.getItem("userData");
+    if (userData) {
+      const parsedData = JSON.parse(userData);
+      setUser(parsedData.name);
+    }
+  }, []);
+
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <AppRouter />
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      {location.pathname !== "/policy" &&
+      location.pathname !== "/logIn" &&
+      location.pathname !== "/singUp" &&
+      location.pathname !== "/about" ? (
+        <div className="globalName">{user}</div>
+      ) : (
+        ""
+      )}
+      <AppRouter />
+      <Footer />
     </>
   );
 }
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,6 +1,16 @@
 import "../style/CourseDetail.scss";
 import CalculateGrades from "./CalculateGrades";
+import { useState, useEffect } from "react";
 export default function Details() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const userData = localStorage.getItem("userData");
+    if (userData) {
+      const parsedData = JSON.parse(userData);
+      setUser(parsedData.name);
+    }
+  }, []);
   return (
     <>
       <section className="detail">
@@ -8,13 +18,19 @@ export default function Details() {
           <div className="detail__header--logo">My Courses</div>
         </div>
         <h1 className="detail__title">
-          Hello, [Name]! Here is your current academic progress:
+          Hello, {user}! Here is your current academic progress:
         </h1>
         <section className="detail__info">
-          <div className="detail__info--item">Course Code</div>
-          <div className="detail__info--item">Teacher Name</div>
-          <div className="detail__info--item">Current grade of Course</div>
-          <div className="detail__info--item">Course total grade</div>
+          <div className="detail__info--item">
+            Course Code
+            <p style={{ fontSize: "1rem", color: "white" }}>Mat 180</p>
+          </div>
+          <div className="detail__info--item">
+            Teacher Name
+            <p style={{ fontSize: "1rem", color: "white" }}>
+              Mugalim Teacherevich
+            </p>
+          </div>
         </section>
 
         <CalculateGrades />
